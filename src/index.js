d1dc17714a122360164e714b69fadcc11f83cd9a
Kyruski <jamescboyett@gmail.com>
2019-01-16 17:36:31 -0600

    Implements partially working videoplayer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 // TODO: Render the `App` component to the DOM
 // import App from './components/App.js';
-import App from '../compiled/src/components/App.js';
+import App from '../src/components/App.js';
 
